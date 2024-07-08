c3606474be68774b7740da1ac9659c778c5cbd5f
akaito1999 <160448291+akaito1999@users.noreply.github.com>
2024-07-08 13:45:29 +0700

    Update __init__.py

--- eventHubTrigger/__init__.py
+++ eventHubTrigger/__init__.py
@@ -6,18 +6,13 @@ import azure.functions as func
 def main(event: func.EventHubEvent):
 
 # Can't be use any more
-	# logging.info('Function triggered to process a message: ', event.get_body())
-    # logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    # logging.info('  SequenceNumber =', event.sequence_number)
-    # logging.info('  Offset =', event.offset)
+    logging.info(f"Function triggered to process a message: {event.get_body().decode()}")
+    logging.info(f"  EnqueuedTimeUtc = {event.enqueued_time}")
+    logging.info(f"  SequenceNumber = {event.sequence_number}")
+    logging.info(f"  Offset = {event.offset}")
 
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
+    result = json.loads(body)
+    logging.info("Python EventGrid trigger processed an event: {}".format(result))
 
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
